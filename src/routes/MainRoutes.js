7f0965a4cfc41ec94bb89c30d0cde8053805cd74
Njeri Muriithi <cnjeri388@gmail.com>
2024-01-27 17:01:59 +0300

    Template Change

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -8,7 +8,7 @@ import MainLayout from 'layout/MainLayout';
 const DashboardDefault = Loadable(lazy(() => import('pages/dashboard')));
 
 // render - sample page
-const SamplePage = Loadable(lazy(() => import('pages/extra-pages/SamplePage')));
+
 
 // render - utilities
 const Typography = Loadable(lazy(() => import('pages/components-overview/Typography')));
@@ -39,10 +39,7 @@ const MainRoutes = {
         }
       ]
     },
-    {
-      path: 'sample-page',
-      element: <SamplePage />
-    },
+
     {
       path: 'shadow',
       element: <Shadow />
